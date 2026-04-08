de76d6db9ed796157beaa2b7c408c1e9aca428df
Derek376 <derek46534@gmail.com>
2026-04-08 12:51:38 +0100

    add orders and payments

--- src/main/java/com/ecommerce/project/repositories/OrderItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/OrderItemRepository.java
@@ -1,12 +1,9 @@
 package com.ecommerce.project.repositories;
 
-import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
