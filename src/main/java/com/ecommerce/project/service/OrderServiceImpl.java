bb116715b4d33ce6280261e94a208a26a6012e8f
Derek376 <derek46534@gmail.com>
2026-07-14 23:48:40 +0100

    implement order update APIs

--- src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -136,4 +136,15 @@ public class OrderServiceImpl implements OrderService {
         orderResponse.setLastPage(pageOrders.isLast());
         return orderResponse;
     }
+
+    @Override
+    public OrderDTO updateOrder(Long orderId, String status) {
+        Order order= orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
+
+        order.setOrderStatus(status);
+        orderRepository.save(order);
+
+        return modelMapper.map(order, OrderDTO.class);
+    }
 }
